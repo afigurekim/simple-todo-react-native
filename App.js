731f176e696681ed0abdcd39a1e3253c2e484fa4
afigurekim <afigurekim@gmail.com>
2019-08-19 01:38:19 +0900

    android fixes

--- App.js
+++ App.js
@@ -45,6 +45,7 @@ export default class App extends Component {
             returnKeyType={"done"}
             autoCorrect={false}
             onSubmitEditing={this._addToDo}
+            underlineColorAndroid={"transparent"}
           />
           <ScrollView contentContainerStyle={styles.toDos}>
             {Object.values(toDos).reverse().map(toDo => (
@@ -73,7 +74,7 @@ export default class App extends Component {
       const parsedToDos = JSON.parse(toDos);
       this.setState({
         loadedToDos: true,
-        toDos: parsedToDos
+        toDos: parsedToDos || {}
       });
     } catch (err) {
       console.log(err);
